9986fe817757ef75a8309068ec409c2aed1af213
Gianni Gambetti <gianni@gambetti.ca>
2015-04-16 12:07:49 -0400

    Adding edge labels, single edge add
    Added documentation for commands, as well as removing create node with edges.

--- rel/__init__.py
+++ rel/__init__.py
@@ -21,17 +21,18 @@ def nb_ui(**kvargs):
 	options.update(kvargs)
 
 
-@nb_ui.command("init")
+@nb_ui.command("init", short_help="create a new graph")
 def init():
 	g = get_graph()
 	g.commit();
 
 
-@nb_ui.command("add")
+@nb_ui.command("add", short_help="create a new node")
 @click.argument("content", required=False)
-@click.option("-p", "--parent", multiple=True)
-@click.option("-c", "--child", multiple=True)
-def add_node(content, parent, child):
+def add_node(content):
+	"""Creates a node and accepts it's content from either the command line,
+	or stdin if no command line content was given. Prints the nodes id to stdout.
+	"""
 
 	if content is None:
 		content = "/dev/stdin"
@@ -43,16 +44,12 @@ def add_node(content, parent, child):
 	graph = get_graph()
 	node = graph.create_node(unicode(content))
 
-	graph.commit() # otherwise node.id == None
-
-	if parent or child:
-		update_relationships(False, parent, child, [node.id])
-
 	graph.commit()
+
 	print(node.id)
 
 
-@nb_ui.command("remove")
+@nb_ui.command("remove", short_help="remove node with id ID from the graph")
 @click.argument("id")
 def remove_node(id):
 
@@ -60,6 +57,7 @@ def remove_node(id):
 	node = graph.node(int(id))
 	graph.remove_nodes(node)
 	graph.commit()
+
 
 def edit_content(content):
 	from tempfile import NamedTemporaryFile
@@ -82,9 +80,13 @@ def edit_content(content):
 		return "".join( temp.readlines() )
 
 
-@nb_ui.command("edit")
+@nb_ui.command("edit", short_help="edit node ID's contents")
+@click.option("-s", "--stdin", is_flag=True, help="take new contents from stdin")
 @click.argument("id")
-def edit_node(id):
+def edit_node(id, stdin):
+	"""Edit a nodes content using 'sensible-editor', or replaces a nodes
+	content with content from stdin.
+	"""
 
 	graph = get_graph()
 
@@ -93,15 +95,22 @@ def edit_node(id):
 		sys.stderr.write("Couldn't find node with id: %s\n" % str(id) )
 		return 1
 
-	new_content = edit_content(node.content)
+	if not stdin:
+		new_content = edit_content(node.content)
+	else:
+		with sys.stdin as f:
+			new_content = "".join( f.readlines() )
+
 
 	node.content = unicode(new_content)
 
 	graph.commit()
 
 
-@nb_ui.command("export")
+@nb_ui.command("export", short_help="output the graph in dot format")
 def data_export():
+	"""Outputs a formatted dot graph to stdout, uses node short description
+	as labels."""
 
 	graph = get_graph()
 
@@ -109,18 +118,26 @@ def data_export():
 
 	for node in graph.nodes():
 		print('\t{id} [label="{short}"]'.format(id=node.id, short=node.short))
-		for child in graph.nodes( Node.id == Edge.tail_id, Edge.head_id == node.id ):
-			print("\t{parent} -> {child}".format(parent=node.id, child=child.id))
+		for out_edge in graph.edges( Edge.head_id == node.id ):
+			print('\t\t{parent} -> {child} [label="{label}"]'.format(parent=node.id, child=out_edge.tail_id, label=out_edge.label))
 
 	print("}")
 
 
 import query, lisp
 
-@nb_ui.command("list")
-@click.option("-t", "--template", default="{id} := {content}")
+@nb_ui.command("list", short_help="query the graph")
+@click.option("-t", "--template", default="{id} := {content}", help="output template for nodes")
 @click.argument("filter", required=False)
 def list_nodes(template, filter):
+	"""Query the graph with FILTER, and output nodes formated as TEMPLATE.
+
+	Template is an arbitrary string without '{' or '}', it exposes the values
+	for the nodes id, content, short content, and time created. These are
+	represented as '{id}', '{content}', '{short}', and '{created}'.
+
+	[TODO: Filter Format]
+	"""
 
 	graph = get_graph()
 
@@ -135,7 +152,10 @@ def list_nodes(template, filter):
 		print( template.format( id=node.id, content=node.content, short=node.short, created=node.created ) )
 
 
-def update_relationships(remove, parent, child, nodes):
+def update_relationships(remove, parent, child, nodes, label):
+
+	if not label:
+		label = u"related"
 
 	graph = get_graph()
 
@@ -149,22 +169,38 @@ def update_relationships(remove, parent, child, nodes):
 
 	for node in graph.nodes( Node.id.is_in(nodes) ):
 		if remove:
-			node.update_parents(remove=parents)
-			node.update_children(remove=children)
+			node.update_parents(remove=parents, label=label)
+			node.update_children(remove=children, label=label)
 		else:
-			node.update_parents(add=parents)
-			node.update_children(add=children)
+			node.update_parents(add=parents, label=label)
+			node.update_children(add=children, label=label)
 
 	graph.commit()
 
 
-@nb_ui.command("related")
-@click.option("--remove", is_flag=True)
-@click.option("-p", "--parent", multiple=True)
-@click.option("-c", "--child", multiple=True)
+@nb_ui.command("related", short_help="edit the edges for a set of nodes")
+@click.option("--remove", is_flag=True, help="remove edges instead of adding them")
+@click.option("-p", "--parent", multiple=True, help="incoming edges from this node (multiple)")
+@click.option("-c", "--child", multiple=True, help="outgoing edges to this node (multiple)")
+@click.option("-l", "--label", default=None, help="the label for the edges")
 @click.argument("nodes", nargs=-1)
-def relations(remove, parent, child, nodes):
-	update_relationships(remove, parent, child, nodes)
+def relations(remove, parent, child, label, nodes):
+	"""Modifies the edges attached to a set of nodes, adding or removing
+	incoming and outgoing edges specified with --parent and --child
+	respectively. The label on the edges is specified by --label, but
+	defaults to 'related'.
+	"""
+	update_relationships(remove, parent, child, nodes, label)
+
+
+@nb_ui.command("edge", short_help="edit the edges for a pair of nodes")
+@click.option("--remove", is_flag=True, help="remove edges instead of adding them")
+@click.option("-l", "--label", default=None, help="the label for the edges")
+@click.argument("parent")
+@click.argument("child")
+def single_edge(remove, parent, child, label):
+
+	update_relationships(remove, [ parent ], [], [ child ], label)
 
 
 if __name__ == "__main__":
